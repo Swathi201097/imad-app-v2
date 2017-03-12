9cc4995660a5ab5deaee522d89017b4782fef2ed
Swathi201097 <swathinagarajan@ymail.com>
2017-03-12 17:38:02 +0530

    [imad-console] Updates ui/creation.js

--- ui/creation.js
+++ ui/creation.js
@@ -20,7 +20,7 @@ var name=document.getElementById('name').value;
 var date=document.getElementById('date').value;
 var phno=document.getElementById('phno').value;
 var email=document.getElementById('email').value;
-request.open('POST','http://swaaaaaathii.imad.hasura-app.io/create-user',true);
+request.open('POST','http://swathi201097.imad.hasura-app.io/create-user',true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username: username, password: password, name: name, date: date, phno: phno, email: email}));
 };
